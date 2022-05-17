79cac9d0c2cced27bd2ed8887fee97bff5fb4ed3
Polina Miftakhudinova <rambeeech@gmail.com>
2022-05-17 14:44:31 +0400

    Fixing default url options

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module GithubQuality
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    routes.default_url_options = { host: ENV['BASE_URL'], protocol: 'https' }
+    routes.default_url_options = { host: ENV['BASE_URL'] || 'test.com' }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
