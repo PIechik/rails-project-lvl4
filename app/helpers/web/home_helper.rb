b98dfca945bc72c8d31bf2fc994365c55b36ba27
Polina Miftakhudinova <rambeeech@gmail.com>
2022-05-20 19:49:19 +0400

    Deleting unnecessary files

--- app/helpers/web/home_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Web
-  module HomeHelper
-  end
-end
--- app/helpers/web/repositories/checks_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Web
-  module Repositories
-    module ChecksHelper
-    end
-  end
-end
